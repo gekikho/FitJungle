5c9f4e170392c34a9dd0e8025a65b0051409c027
root <root@LAPTOP-PV8O7829>
2023-11-10 10:27:41 +0000

    lakakakakaka

--- src/component/script.js
+++ src/component/script.js
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import background from "./images/background.mp4";
+import exo from "../data/exo.json"
 
 function Script (){
     const [Page,setPage] = useState("home");
     const [programme,setProgramme] = useState(0);
+    
     const plan = {
         style : " ",
+        goal : "",
         chest : [],
         back : [],
         shoulders : [],
@@ -14,6 +17,7 @@ function Script (){
         quadriceps : [],
         hamstring : []
     }
+    {console.log(plan)}
 
 
     
@@ -50,102 +54,84 @@ function Script (){
               ) : null}
               {Page === "programme" ? (
             <>
-            <h2 id = " ">BE the STRONGEEEEEST</h2>
             
 
             {programme === 0 ?(
-                <>
                 
-                <p>How many times a week are you ready to train? </p>
-                <div>
+                <div className="block">
+                <div className="quizz">
+                <p id = "question">How many times a week are you ready to train? </p>
                 <form>
-                        <button id = "reponse" onclick={plan["style"] = "full-body"}>2 times</button>
-                        <button id = "reponse" onclick={plan["style"] = "ppl"}>3 times</button>
-                        <button id = "reponse" onclick={plan["style"] = "uper-lower"}>4 times</button>
-                        <button id = "reponse" onclick={plan["style"] = "split"}>5 times</button>
-                        <button id = "reponse" onclick={plan["style"] = "ppl+"}>6 times</button>
+                        <button id = "reponse"  type = "button" onClick={() => plan["style"] = "full-body"}>2 times</button>
+                        <button id = "reponse" type = "button"  onClick={() =>plan["style"] = "ppl"}>3 times</button>
+                        <button id = "reponse" type = "button"  onClick={() =>plan["style"] = "uper-lower"}>4 times</button>
+                        <button id = "reponse" type = "button"  onClick={() =>plan["style"] = "split"}>5 times</button>
+                        <button id = "reponse" type = "button"  onClick={() =>plan["style"] = "ppl+"}>6 times</button>
 
                 </form>
+                <p>I advise you to train 3-4-5 per week</p>
                 </div>
-                <p>I should you not train much but not a little to so 3-4-5 per week is good</p>
-                <button onclick={() => setProgramme(1)}>Next</button>
-                </>
+                
 
+                <button class = "next" onClick={() => setProgramme(1)}>Next</button>
+                </div>
+                
 
             ): null}
             {programme === 1 ?(
-                <>
+                <div className="block">
                 
-                <p></p>
-                <div>
+                
+                <div className="quizz">
+                <p id = "question">What is your objective </p>
                 <form>
-                        <button id = "reponse" onclick="verifierReponse()">2 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">3 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">4 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">5 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">6 times</button>
+                        <button id = "reponse" type = "button"  onClick={() =>plan["goal"] = "muscle"}>Gain muscle</button>
+                        <button id = "reponse" type = "button"  onClick={() =>plan["goal"] = "strength"}>Gain strength</button>
+                        <button id = "reponse" type = "button"  onClick={() =>plan["goal"] = "weight"}>Losing weight</button>
+                        <button id = "reponse" type = "button"  onClick={() =>plan["goal"] = "endurance"}>Gain endurance</button>
 
                 </form>
                 </div>
-                <button onclick={() => setProgramme(2)}>Next</button>
-                </>
+                <button class = "next" onClick={() => setProgramme(2)}>Next</button>
+                </div>
+
+            
 
 
             ): null}
 
-            {programme === 1 ?(
-                <>
-                
-                <p></p>
-                <div>
-                <form>
-                        <button id = "reponse" onclick="verifierReponse()">2 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">3 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">4 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">5 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">6 times</button>
-
-                </form>
-                </div>
-                <button onclick={() => setProgramme(2)}>Next</button>
-                </>
-
-
-            ): null}
             {programme === 2 ?(
-                <>
+                <div className="block">
                 
-                <p></p>
-                <div>
+                
+                <div className="quizz">
+                <p id = "question">Do you have :</p>
                 <form>
-                        <button id = "reponse" onclick="verifierReponse()">2 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">3 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">4 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">5 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">6 times</button>
+                        <button id = "reponse" type = "button"  onClick={() =>(plan["chest"].append(exo[0].long_arms))(plan["back"].append(exo[1].long_arms))(plan["shoulders"].append(exo[2].long_arms))(plan["biceps"].append(exo[3].long_arms))(plan["triceps"].append(exo[4].long_arms))(plan["quadriceps"].append(exo[5].long_arms))(plan["hamstring"].append(exo[6].long_arms))}>Long arms</button>
+                        <button id = "reponse" type = "button"  onClick={() => (plan["chest"].append(exo[0].short_arms))(plan["back"].append(exo[1].short_arms))(plan["shoulders"].append(exo[2].short_arms))(plan["biceps"].append(exo[3].short_arms))(plan["triceps"].append(exo[4].short_arms))}>Short arms</button>
 
                 </form>
                 </div>
-                <button onclick={() => setProgramme(3)}>Next</button>
-                </>
+                <button class = "next" onClick={() => setProgramme(3)}>Next</button>
+                </div>
 
 
             ): null}
             {programme === 3 ?(
                 <>
                 
-                <p></p>
-                <div>
+                <div className="quizz">
+                <p id = "question">ergegzgzgzh</p>
                 <form>
-                        <button id = "reponse" onclick="verifierReponse()">2 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">3 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">4 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">5 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">6 times</button>
+                        <button id = "reponse" type = "button"  onClick="verifierReponse()">2 times</button>
+                        <button id = "reponse" type = "button"  onClick="verifierReponse()">3 times</button>
+                        <button id = "reponse" type = "button"  onClick="verifierReponse()">4 times</button>
+                        <button id = "reponse" type = "button"  onClick="verifierReponse()">5 times</button>
+                        <button id = "reponse" type = "button"  onClick="verifierReponse()">6 times</button>
 
                 </form>
                 </div>
-                <button onclick={() => setProgramme(4)}>Next</button>
+                <button class = "next" onclick={() => setProgramme(4)}>Next</button>
                 </>
 
 
@@ -153,18 +139,55 @@ function Script (){
             {programme === 4 ?(
                 <>
                 
-                <p></p>
-                <div>
+                
+                <div className="quizz">
+                <p id = "question">ergehebse</p>
                 <form>
-                        <button id = "reponse" onclick="verifierReponse()">2 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">3 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">4 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">5 times</button>
-                        <button id = "reponse" onclick="verifierReponse()">6 times</button>
+                        <button id = "reponse" type = "button"  onclick="verifierReponse()">2 times</button>
+                        <button id = "reponse" type = "button"  onclick="verifierReponse()">3 times</button>
+                        <button id = "reponse" type = "button"  onclick="verifierReponse()">4 times</button>
+                        <button id = "reponse" type = "button"  onclick="verifierReponse()">5 times</button>
+                        <button id = "reponse" type = "button"  onclick="verifierReponse()">6 times</button>
 
                 </form>
                 </div>
-                <button onclick={() => setProgramme(5)}>Next</button>
+                <button class = "next" onclick={() => setProgramme(5)}>Next</button>
+                </>
+
+
+            ): null}
+            {programme === 5 ?(
+                <>
+                
+                <p id = "question"></p>
+                <div className="quizz">
+                <form>
+                        <button id = "reponse" type = "button"  onclick="verifierReponse()">2 times</button>
+                        <button id = "reponse" type = "button"  onclick="verifierReponse()">3 times</button>
+                        <button id = "reponse" type = "button"  onclick="verifierReponse()">4 times</button>
+                        <button id = "reponse" type = "button"  onclick="verifierReponse()">5 times</button>
+                        <button id = "reponse" type = "button"  onclick="verifierReponse()">6 times</button>
+
+                </form>
+                </div>
+                <button class = "next" onclick={() => setProgramme(6)}>Next</button>
+                </>
+
+
+            ): null}
+
+            {programme === 6 ?(
+                <>
+                
+                <p id = "question">programme en  </p>
+                <div className="quizz">
+                <form>
+                        {/* <p>{plan["chest"]} <br></br> {plan["back"]} <br></br> {plan["shoulders"]} <br></br> {plan["biceps"]} <br></br> {plan["triceps"]} <br></br> {plan["quadriceps"]} {plan["hamstring"]}</p> */}
+
+
+                </form>
+                </div>
+                <button class = "next" onclick={() => setPage("home")}>Next</button>
                 </>
 
 
